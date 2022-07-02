2b58ea1de1f79b33ded8a07da65d8e250b395160
matiSzewczyk <szewczyk.mateusz16@gmail.com>
2022-07-02 14:24:18 +0200

    First commit

---
+++ app/src/main/java/com/app/gw2_pvp_hub/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.app.gw2_pvp_hub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gw2_pvp_hub.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
