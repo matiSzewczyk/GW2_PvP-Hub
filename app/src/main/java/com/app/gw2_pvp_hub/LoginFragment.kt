b3e21aaae17d1c8bea3fc3278ce0e2c38a80a5a0
matiSzewczyk <szewczyk.mateusz16@gmail.com>
2022-07-02 22:49:09 +0200

    Add onDestroy and onViewCreate

--- app/src/main/java/com/app/gw2_pvp_hub/LoginFragment.kt
+++ app/src/main/java/com/app/gw2_pvp_hub/LoginFragment.kt
@@ -1,18 +1,37 @@
 package com.app.gw2_pvp_hub
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.gw2_pvp_hub.R
 import com.example.gw2_pvp_hub.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment(R.layout.fragment_login){
 
-    private lateinit var binding: FragmentLoginBinding
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLoginBinding.inflate(layoutInflater, container, false)
+        return binding!!.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        
+        binding!!.loginButton.setOnClickListener {
+            // TODO: implement login logic  
+        }
+    }
 
-        binding = FragmentLoginBinding.bind(view)
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
