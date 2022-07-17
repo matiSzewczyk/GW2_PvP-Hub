ac75f18785367b9ac987e0b55ec07ec0b88084f1
matiSzewczyk <szewczyk.mateusz16@gmail.com>
2022-07-17 18:56:47 +0200

    Add bottom nav bar, tweak themes

--- app/src/main/java/com/app/gw2_pvp_hub/MainActivity.kt
+++ app/src/main/java/com/app/gw2_pvp_hub/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.app.gw2_pvp_hub.databinding.ActivityMainBinding
 import com.app.gw2_pvp_hub.ui.viewModels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
@@ -17,13 +19,19 @@ class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     private val viewModel: MainViewModel by viewModels()
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.findNavController()
+
+        binding.bottomNavigationView.setupWithNavController(navController)
 
         viewModel.checkLoggedIn()
 
---
+++ app/src/main/java/com/app/gw2_pvp_hub/ui/fragments/ChatFragment.kt
@@ -0,0 +1,32 @@
+package com.app.gw2_pvp_hub.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.app.gw2_pvp_hub.databinding.FragmentChatBinding
+
+class ChatFragment : Fragment() {
+
+    private var _binding: FragmentChatBinding? = null
+    private val binding get() = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentChatBinding.inflate(layoutInflater, container, false)
+        return binding!!.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
